f12ec01ec3be9e28181a28476c22033d8c2af674
Arpit123139 <arpitgajya2001@gmail.com>
2022-10-09 23:27:54 +0530

    Initial Commit

--- app/src/main/java/com/example/assignment1/MainActivity.kt
+++ app/src/main/java/com/example/assignment1/MainActivity.kt
@@ -52,7 +52,7 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onCancelled(error: DatabaseError) {
-                TODO("Not yet implemented")
+
             }
 
         })
